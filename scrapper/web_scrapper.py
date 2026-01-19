84ecec40080d3a36f848e2ace59911fc88311b2d
Samuel <samuelpierrej@gmail.com>
2026-01-19 15:36:25 -0300

    refactor: new methods to extract data from web

--- scrapper/web_scrapper.py
+++ scrapper/web_scrapper.py
@@ -2,43 +2,8 @@ from playwright.sync_api import sync_playwright
 import locale
 from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
-from exceptions import  DateTimeConfigError
-
-def get_week_extremes(target_date: datetime = None) -> str:
-    # Se não passar data, usa a de hoje
-    if target_date is None:
-        target_date = datetime.now().date()
-
-    monday = target_date - timedelta(days=target_date.weekday())
-    sunday = monday + timedelta(days=6)
-
-    meses = {
-        1: "ENERO", 2: "FEBRERO", 3: "MARZO", 4: "ABRIL", 
-        5: "MAYO", 6: "JUNIO", 7: "JULIO", 8: "AGOSTO", 
-        9: "SEPTIEMBRE", 10: "OCTUBRE", 11: "NOVIEMBRE", 12: "DICIEMBRE"
-    }
-
-    # QUANDO A SEMANA COMEÇAR NO MES E TERMINAR NO MESMO MES
-    if monday.month == sunday.month:
-        nome_mes = meses[monday.month]
-        return f"{monday.day}-{sunday.day} DE {nome_mes}"
-    # QUANDO A SEMANA COMEÇAR NO MES E TERMINAR NO OUTRO MES
-    else:
-        mes_segunda = meses[monday.month]
-        mes_domingo = meses[sunday.month]
-        return f"{monday.day} DE {mes_segunda} A {sunday.day} DE {mes_domingo}"
 
 
-
-def scrape_data(page) -> list[str]:
-    content = []
-    html = page.content()
-    soup = BeautifulSoup(html, 'html.parser')
-    
-    for header in soup.find_all(['h1', 'h2', 'h3']):
-        content.append(header.text.strip())
-
-    return content
 
 class DataScrapper:
     def __init__(self):
@@ -47,29 +12,124 @@ class DataScrapper:
         except:
             locale.setlocale(locale.LC_ALL, 'es-MX')
 
-        try:
-            self.year = datetime.now().year
-            self.month = datetime.now().strftime('%B')
-            self.week = datetime.now().isocalendar()[1]
-            self.week_of_the_meeting = get_week_extremes()
-            self.url = f"https://wol.jw.org/es/wol/meetings/r4/lp-s/{self.year}/{self.week}"
-            self.meeting_selector = f"{self.week_of_the_meeting}"
-        except Exception as e:
-            raise DateTimeConfigError(e)
 
+    def open_browser(self):
+        self.playwright = sync_playwright().start()
+        
+        self.browser = self.playwright.chromium.launch(headless=False)
 
+        self.context = self.browser.new_context()
+        page = self.context.new_page()
 
-    def open_browser_and_scrappe_data(self) -> list[str]:
-        with sync_playwright() as p:            
-            # headless=False para visualizar a janela
-            browser = p.chromium.launch(headless=False)
-            page = browser.new_page()
-            page.goto(self.url)
-            page.get_by_role("link", name= self.meeting_selector).click()
-
-            data = scrape_data(page)
-            print(data)
-            browser.close()
-
-            return data
+        return page
     
+    @staticmethod
+    def get_week_extremes() -> str:
+        target_date = datetime.now().date()
+
+        monday = target_date - timedelta(days=target_date.weekday())
+        sunday = monday + timedelta(days=6)
+
+        meses = {
+            1: "ENERO", 2: "FEBRERO", 3: "MARZO", 4: "ABRIL", 
+            5: "MAYO", 6: "JUNIO", 7: "JULIO", 8: "AGOSTO", 
+            9: "SEPTIEMBRE", 10: "OCTUBRE", 11: "NOVIEMBRE", 12: "DICIEMBRE"
+        }
+
+        # QUANDO A SEMANA COMEÇAR NO MES E TERMINAR NO MESMO MES
+        if monday.month == sunday.month:
+            nome_mes = meses[monday.month]
+            return f"{monday.day}-{sunday.day} DE {nome_mes}".lower()
+        # QUANDO A SEMANA COMEÇAR NO MES E TERMINAR NO OUTRO MES
+        else:
+            mes_segunda = meses[monday.month]
+            mes_domingo = meses[sunday.month]
+            return f"{monday.day} DE {mes_segunda} A {sunday.day} DE {mes_domingo}".lower()
+
+    @staticmethod
+    def scrape_data(page) -> list[str]:
+        content = []
+        html = page.content()
+        soup = BeautifulSoup(html, 'html.parser')
+        
+        for header in soup.find_all(['h1', 'h2', 'h3']):
+            content.append(header.text.strip())
+
+        return content
+
+
+    def extract_this_month(self, page):
+        data = []
+        
+        current_month = datetime.now().strftime('%B').lower()
+        current_year = datetime.now().year
+        current_week_text = self.get_week_extremes() 
+
+        link = f"https://wol.jw.org/es/wol/library/r4/lp-s/biblioteca/guía-de-actividades/guía-de-actividades-{current_year}/{current_month}"
+        page.goto(link)
+
+        items = page.locator("#materialNav nav ul li a.cardContainer").all()
+
+        valid_links = []
+        found_current_week = False
+
+        for item in items:
+            item_text = item.inner_text().lower()
+
+            if current_week_text.lower() in item_text:
+                found_current_week = True
+            
+            if found_current_week:
+                href = item.get_attribute("href")
+                full_url = f"https://wol.jw.org{href}" if href.startswith('/') else href
+                valid_links.append(full_url)
+
+        for url in valid_links:
+            page.goto(url)
+            data.append(self.scrape_data(page))
+        
+        return data
+
+    
+
+    def extract_this_week(self, page) -> list[str]:
+        """
+        EXTRAIR AS REUNIOES DESTA SEMANA
+        """
+        link = "https://wol.jw.org/es/wol/h/r4/lp-s"
+
+        page.goto(link)
+
+        page.click("#menuToday")
+    
+        page.wait_for_load_state("networkidle")
+
+        current_week = self.get_week_extremes() 
+        
+        links = page.locator("ul.directory.navCard li.todayItem a.cardContainer")
+    
+
+        count = links.count()
+        
+        for i in range(count):
+            link = links.nth(i)
+            text = link.inner_text()
+            
+            if current_week.lower() in text.lower():
+                link.click()
+                break
+
+        data = self.scrape_data(page)
+
+        return data
+    
+    @staticmethod
+    def extract_all_available_weeks(self, page):
+        current_year = datetime.now().year
+        link = f"https://wol.jw.org/es/wol/library/r4/lp-s/biblioteca/guía-de-actividades/guía-de-actividades-{current_year}"
+
+
+
+main = DataScrapper()
+page = main.open_browser()
+main.extract_this_week(page)
